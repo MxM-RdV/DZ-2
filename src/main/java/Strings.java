88059fde951d4473b5695e577bfdb7be702fa653
mxm_rdv <mxm-rdv@yandex.ru>
2024-11-10 18:26:10 +0300

    My first commit

---
+++ src/main/java/Strings.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+
+public class Strings {
+    public static void main(String[] args) {
+        int x = 5;
+        String s = "Hello";
+        String space = " ";
+        String name = "Max";
+        System.out.println(s+space+name);
+        System.out.println("Hello"+ " "+ "Maxim");
+        System.out.println("Ny number is " + x);
+    }
+}
