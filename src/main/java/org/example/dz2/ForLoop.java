060b9613a8e9b91e93cd421cc9c664c91381af0c
mxm_rdv <mxm-rdv@yandex.ru>
2024-11-14 02:19:19 +0300

    My second commit

---
+++ src/main/java/org/example/dz2/ForLoop.java
@@ -0,0 +1,9 @@
+package org.example.dz2;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int i = 10;i>=0;i = i-5) {
+            System.out.println("Hello "+i);
+        }
+    }
+}
