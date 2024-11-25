8484c4148fec495d4559c993c07df88a581f35b0
mxm_rdv <mxm-rdv@yandex.ru>
2024-11-25 18:21:04 +0300

    Delo sdelano

---
+++ src/main/java/org/example/dz5/dninedeli.java
@@ -0,0 +1,38 @@
+package org.example.dz5;
+
+import java.util.Scanner;
+
+public class dninedeli {
+
+
+    public static String daysnedeli(int number) {
+        return switch (number) {
+            case 1 -> "Понедельник";
+            case 2 -> "Вторник";
+            case 3 -> "Среда";
+            case 4 -> "Четверг";
+            case 5 -> "Пятница";
+            case 6 -> "Суббота";
+            case 7 -> "Воскресенье";
+            default -> "Некорректное значение. Введите число от 1 до 7.";
+        };
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Введите число (от 1 до 7): ");
+            int number = scanner.nextInt();
+            System.out.println(daysnedeli(number));
+
+            System.out.print("Хотите ввести еще одно число? (да/нет): ");
+            String again = scanner.next().trim().toLowerCase();
+            if (!again.equals("да")) {
+                System.out.println("Программа завершена.");
+                break;
+            }
+        }
+        scanner.close();
+    }
+}
+
